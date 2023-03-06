c7115f81ee8a512f6b89569c6ac3cc7c87615525
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2023-03-06 16:55:51 +0100

    add row

--- reportsizedeltas/reportsizedeltas.py
+++ reportsizedeltas/reportsizedeltas.py
@@ -332,6 +332,8 @@ class ReportSizeDeltas:
                 column_number += 1
                 # Populate the row with data
                 for sketch in fqbn_data[self.ReportKeys.sketches]:
+                    # Add a row to the report
+                    row = ["" for _ in range(len(summary_report_data[0]))]
                     row[0] = sketch[self.ReportKeys.name]
                     summary_report_data.append(row)
                     # Add the absolute memory data to the cell
