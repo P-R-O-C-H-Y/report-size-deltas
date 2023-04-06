7d7a38dc14594e0e7b3ccbd4463b4995ce2c7dc0
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2023-04-06 08:16:49 +0200

    Update reportsizedeltas.py

--- reportsizedeltas/reportsizedeltas.py
+++ reportsizedeltas/reportsizedeltas.py
@@ -153,6 +153,8 @@ class ReportSizeDeltas:
         """Scan the repository's pull requests and comment memory usage change reports where appropriate."""
         # Get the repository's pull requests
         input_pr_number = token=os.environ["INPUT_PR-NUMBER"]
+        print(input_pr_number)
+        
         if input_pr_number:
             api_data = self.api_request(request="repos/" + self.repository_name + "/pulls/" + input_pr_number)
             pr_data = api_data["json_data"]
