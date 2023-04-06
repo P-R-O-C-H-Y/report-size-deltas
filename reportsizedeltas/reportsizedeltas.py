53b0b14f0771fe7a1cccea4b9ece6c17469a384e
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2023-04-06 08:25:14 +0200

    Update reportsizedeltas.py

--- reportsizedeltas/reportsizedeltas.py
+++ reportsizedeltas/reportsizedeltas.py
@@ -152,7 +152,7 @@ class ReportSizeDeltas:
     def report_size_deltas_from_workflow_artifacts(self):
         """Scan the repository's pull requests and comment memory usage change reports where appropriate."""
         # Get the repository's pull requests
-        input_pr_number =os.environ["INPUT_PR-NUMBER"]
+        input_pr_number = os.environ["INPUT_PR-NUMBER"]
         print(input_pr_number)
 
         if input_pr_number:
