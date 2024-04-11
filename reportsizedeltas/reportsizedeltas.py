54926e41cb64200b8fe7913ea9ae6ed097771419
Jan Prochazka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2024-04-11 22:38:43 +0200

    Update reportsizedeltas.py

--- reportsizedeltas/reportsizedeltas.py
+++ reportsizedeltas/reportsizedeltas.py
@@ -768,11 +768,8 @@ def generate_summary_html_table(row_list):
     html_table = html_table + "<tr>" + "".join(["<th>" + str(cell) + "</th>" for cell in row_list[0]]) + "</tr>\n"
     #html_table = html_table + "<tr>" + "".join(["<th colspan='2' align='center'>" + str(cell) + "</th>" if index != 0 else "<th>" + str(cell) + "</th>" for index, cell in enumerate(row_list[0])]) + "</tr>\n"
 
-    align='right'
     # Add data rows
     for row in row_list[1:]:
-        html_table = html_table + "<tr>" + "".join(["<td>" + str(cell) + "</td>" for cell in row]) + "</tr>\n"
-
         html_table = html_table + "<tr>" + "".join(["<td align='right'>" + str(cell) + "</td>" if index != 0 else "<td>" + str(cell) + "</td>" for index, cell in enumerate(row)]) + "</tr>\n"
 
     html_table = html_table + "</table>\n"
